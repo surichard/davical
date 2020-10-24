9458cd9b23b1459668281f982f6c54170d964b0e
Richard <surichard@users.noreply.github.com>
2020-10-25 05:15:06 +0800

    Update config.php

--- davical/config.php
+++ davical/config.php
@@ -2,11 +2,11 @@
 
 $c->authenticate_hook['call'] = 'IMAP_PAM_check';
 $c->authenticate_hook['config'] = array(
-    'imap_url'   => '{example.com:143/imap/tls/}',
-    'email_base' => 'example.com'
+    'imap_url'   => '{ixecloud.com:143/imap/tls/}',
+    'email_base' => 'ixecloud.com'
 );
 
-$c->admin_email  = 'admin@example.com';
+$c->admin_email  = 'admin.ixe@ixecloud.com';
 $c->pg_connect[] = array( 'dsn' => 'pgsql:dbname=davical port=5432 host=postgres', 'dbuser' => 'davical', 'dbpass' => 'davical' );
 
 include('drivers_imap_pam.php');
